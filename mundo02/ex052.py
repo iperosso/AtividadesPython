3bee3fd4fec20a76b42331ee36f14c9186ea3759
iperosso <iago_perosso@hotmail.com>
2022-07-20 17:36:29 -0300

    atualizacao-cod

--- mundo02/ex052.py
+++ mundo02/ex052.py
@@ -1,16 +1,11 @@
 """ FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E DIGA SE ELE É OU NÃO UM NÚMERO PRIMO"""
 
-num = int(input('Digite o número que você deseja analisar: '))
+num = int(input('Digite um número inteiro para analisar'))
 count = 0
 for i in range(1, num + 1):
     if num % i == 0:
-        print('\033[33m', end=' ')
         count += 1
-    else:
-        print('\033[31m', end=' ')
-    print(f'{i}', end=' ')
-print(f'\n\033[mO número {num} foi dividido {count}x')
-if count <= 2:
-    print('Por isso ele é um número PRIMO')
+if count == 2:
+    print(f'O número {num} É PRIMO')
 else:
-    print('Por isso ele NÃO È UM NÚMERO PRIMO')
+    print(f'O número {num} NÃO É PRIMO')
