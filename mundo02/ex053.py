e340e8d6c36391bb31dc274b43aea47e4e671716
iperosso <iago_perosso@hotmail.com>
2022-07-21 19:15:32 -0300

    exercicio-realizado

---
+++ mundo02/ex053.py
@@ -0,0 +1,14 @@
+""" CRIE UM PROGRAMA QUE LEIA UMA FRASE QUALQUER E DIGA SE ELA É UM PALINDROMO, DESCONSIDERE OS 
+    ESPAÇO """
+
+frase = str(input('Digite uma frase para analisar: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'A frase {junto} lida ao contrário fica {inverso}')
+if junto == inverso:
+    print('Isso é um palindromo')
+else:
+    print('Isso não é um palindromo')
