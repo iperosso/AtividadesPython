6ba21c03d1ba63bd9aebc374efbd658fb5f3f06e
Iago Perosso <84355461+iperosso@users.noreply.github.com>
2024-02-07 12:51:52 -0300

    ex 79

--- mundo03/ex079.py
+++ mundo03/ex079.py
@@ -2,3 +2,18 @@
 # LISTA, CASO O NUMERO JA ESTEJA NA LISTA, ELE NÃO SERÁ ADICIONADO. NO FINAL, SERÃO EXIBIDOS
 # TODOS OS VALORES UNICOS DIGITADOS EM ORDEM CRESCENTE.
 
+lista = list()
+while True:
+    num = int(input('Digite um número: '))
+    if num not in lista:
+        lista.append(num)
+        print(f'O número {num} foi adicionado com sucesso')
+    else:
+        print(f'Já havia o número {num} na lista')
+    resp = input(str(f'Deseja continuar? [S/N] '))
+    if resp not in 'sSnN':
+        resp = input(str('Resposta inválida, Deseja continuar? [S/N]'))
+    elif resp in 'nN':
+        break
+print(lista)
+print('Programa encerrado.')
